a34b51524eb400c95feace724e89e1a5eba968b2
camilocerquera <jc.cerquera10@uniandes.edu.co>
2017-10-16 13:13:41 -0500

    notificaciones

--- public/js/controllers/notificacionesController.js
+++ public/js/controllers/notificacionesController.js
@@ -2,18 +2,59 @@ angular.module('app').controller('notificacionesController', function notificaci
 
   var ctrl = this;
 
-  $scope.todasNotificaciones = {};
+  $scope.todasNotificaciones = [];
 
   $scope.config = {
-    itemsPerPage: 2,
-    maxPages: 3,
+    itemsPerPage: 10,
     fillLastPage: false
   }
 
-  $http.get('/notification')
-    .then(function (response) {
-      $scope.todasNotificaciones = response.data;
-    });
+  initController();
+
+  function initController() {
+    consultarTodasNotificaciones()
+  }
+
+  function consultarTodasNotificaciones() {
+    $http.get('/notification')
+      .then(function (response) {
+        $scope.todasNotificaciones = response.data;
+      });
+  }
+
+
+
+  $scope.filtrarxDias = function filtrarxDias(dias) {
+    $http.get('/notification')
+      .then(function (response) {
+        notificaciones = response.data;
+        $scope.todasNotificaciones = [];
+        for(i = 0; i<notificaciones.length;i++){
+          fechaSinUTC =  notificaciones[i].fecha.replace('Z', '');
+          fechaNotificacion = new Date(fechaSinUTC);
+          fechaInicial = sumarDias(new Date(),-dias);
+          fechaNotificacion.setHours(0,0,0,0);
+          fechaInicial.setHours(0,0,0,0);
+          if (fechaNotificacion >= fechaInicial){
+            $scope.todasNotificaciones.push(notificaciones[i]);
+          }
+        }
+      });
+
+  }
+
+  function sumarDias(fecha, dias) {
+    fecha.setDate(fecha.getDate() + dias);
+    return fecha;
+  }
+
+  $scope.obtenerImagenCanal = function obtenerImagenCanal(canal) {
+    if (canal == 1){
+      return 'img/booking.png';
+    }else if(canal == 2){
+      return 'img/expedia.png';
+    }
+  }
 
 
   // function setPage(page) {
